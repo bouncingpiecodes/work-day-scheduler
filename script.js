15944dadfcec34cece7beafc277eab1dc42f571d
“kara <bouncingpie@icloud.com>
2022-10-04 20:19:58 -0400

    fixed the timing

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ window.onload = function () {
   var current = new Date();
   var displayDate = moment(current).format("dddd, MMMM Do");
   $("#currentDay").html(displayDate);
+  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  workHours.forEach(function (hour) {
+    var displayHour = moment(new Date().setHours(hour)).format("h a");
+    $(".container").html(
+      $(".container").html() +
+        `<div class="row">
+            <div class="hour">${displayHour}</div>
+            <textarea aria-label="description" class="description present">test</textarea>
+            <button class="saveBtn">Save</button>
+        </div>`
+    );
+  });
 };
