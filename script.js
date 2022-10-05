a8d26b63147018ac991cb4c4eb4e80abdee7817b
“kara <bouncingpie@icloud.com>
2022-10-04 20:30:37 -0400

    adding if else for time past and present

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ window.onload = function () {
   $("#currentDay").html(displayDate);
   var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   workHours.forEach(function (hour) {
-    var displayHour = moment(new Date().setHours(hour)).format("h a");
+    var theDate = new Date(hour);
+    var displayHour = moment(theDate.setHours(hour)).format("h a");
+    var timeClass;
+    if (theDate < current) {
+      timeClass = "past";
+    } else if (theDate >= current) {
+    }
     $(".container").html(
       $(".container").html() +
         `<div class="row">
             <div class="hour">${displayHour}</div>
-            <textarea aria-label="description" class="description present">test</textarea>
+            <textarea aria-label="description" class="description">test</textarea>
             <button class="saveBtn">Save</button>
         </div>`
     );
