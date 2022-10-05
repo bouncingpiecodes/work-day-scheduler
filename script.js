d0982c91c3eab3c4ebb0f975eff8661272e77be3
“kara <bouncingpie@icloud.com>
2022-10-04 21:13:56 -0400

    added JSON parse for local storage to store events

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+var storage = JSON.parse(localStorage.getItem("storage")) ?? {};
 window.onload = function () {
   var current = new Date();
   var displayDate = moment(current).format("dddd, MMMM Do");
   $("#currentDay").html(displayDate);
-  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var workHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
   workHours.forEach(function (hour) {
-    var theDate = new Date(hour);
-    var displayHour = moment(theDate.setHours(hour)).format("h a");
+    var theDate = new Date().setHours(hour);
+    var displayHour = moment(theDate).format("h a");
+    var description = !!storage[hour] ? storage[hour] : "";
     var timeClass;
     if (theDate < current) {
       timeClass = "past";
-    } else if (theDate >= current) {
+    } else if (
+      theDate >= current &&
+      theDate < current.getTime() + 60 * 60 * 1000
+    ) {
+      timeClass = "present";
+    } else {
+      timeClass = "future";
     }
     $(".container").html(
       $(".container").html() +
         `<div class="row">
             <div class="hour">${displayHour}</div>
-            <textarea aria-label="description" class="description">test</textarea>
-            <button class="saveBtn">Save</button>
+            <textarea id="description-${hour}" aria-label="description" class="description ${timeClass}">${description}</textarea>
+            <button onclick="save(${hour})" class="saveBtn">Save</button>
         </div>`
     );
   });
 };
+function save(hour) {
+  var description = $(`#description-${hour}`).val();
+  storage[hour] = description;
+  localStorage.setItem("storage", JSON.stringify(storage));
+  alert("Record Saved");
+}
